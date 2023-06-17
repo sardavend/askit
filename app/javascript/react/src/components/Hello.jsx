b653a9d3146809cdf2a5a94d3c366b064040b21d
juanca sardin <juan.carlos.sardin@gmail.com>
2023-06-16 22:11:25 -0400

    initial commit

---
+++ app/javascript/react/src/components/Hello.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+
+const Hello = () => {
+  return (<div className='text-white'>Hello Rails 7</div>)
+}
+
+export default Hello
---
+++ app/javascript/react/src/index.js
@@ -0,0 +1,4 @@
+import { define } from 'remount'
+import Hello from "./components/Hello"
+
+define({'ask-it-hello': Hello})
