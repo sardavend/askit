b2016e672d77901401c48828080a63eb6dc5d230
juanca sardin <juan.carlos.sardin@gmail.com>
2023-06-21 23:31:28 -0400

    Refactor definitions for react components

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,4 +1,8 @@
 import { define } from 'remount'
 import Hello from "./components/Hello"
+import PdfViewer from "./components/PdfViewer"
 
-define({'ask-it-hello': Hello})
+define({
+  'ask-it-hello': Hello,
+  'ask-it-pdf-viewer': PdfViewer
+})
