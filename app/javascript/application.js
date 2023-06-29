2ad7b1221bd945034f51a3dccdc5e590b7caf79b
juanca sardin <juan.carlos.sardin@gmail.com>
2023-06-28 23:08:24 -0400

    Add base custom confirm dialogs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "./react/src/index.js"
 
+Turbo.setConfirmMethod((message, element) => {
+    const dialog = document.getElementById("turbo-confirm")
+    dialog.showModal()
+    dialog.querySelector("p").textContent = message
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener('close', () => {
+            resolve(dialog.returnValue === 'confirm')
+        }, {once: true})
+    })
+})
+
